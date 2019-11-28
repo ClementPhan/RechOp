e9679594e6552c6a93165d2a22af27fc52f30d72
Clément Phan <phanou74370@gmail.com>
2019-11-28 15:31:55 +0100

    Output vers le fichier demande

--- RechOP.cpp
+++ RechOP.cpp
@@ -2,17 +2,57 @@
 //
 
 #include "RechOP.h"
-#include "parseur/parseur.h"
 
-using namespace std;
+int output(vector<tournee> tournees, vector<groupe> groupes, vector<int> fournisseurs_sous_traites) {
+
+	string const nomFichier("C:/Users/hugues/Desktop/KIRO/solution.txt");
+	ofstream monFlux(nomFichier.c_str());
+
+	// Sous-traitants
+	monFlux << "x" << fournisseurs_sous_traites.size() << " f";
+	for (int i = 0; i < fournisseurs_sous_traites.size(); i++) {
+		monFlux << " " << fournisseurs_sous_traites[i];
+	}
+	monFlux << std::endl;
+
+	// Nombre de Tournées
+	monFlux << "y " << tournees.size() << std::endl;
+	
+	// Groupes
+	monFlux << "z " << groupes.size() << std::endl;
+
+	for (int i = 0; i < groupes.size(); i++) {
+		monFlux << "C " << i << " n " << groupes[i].nombre_de_fournisseurs << " f";
+		for (int j = 0; j < groupes[i].nombre_de_fournisseurs; j++) {
+			monFlux << " " << groupes[i].fournisseurs;
+		}
+		monFlux << std::endl;
+	}
+
+	// Tournées
+	for (int i = 0; i < tournees.size(); i++) {
+		monFlux << "P " << i << " g " << tournees[i].groupe << " s " << tournees[i].semaine << " n " << tournees[i].semaine << " n " << tournees[i].nombre_de_fournisseurs;
+		for (int j = 0; j < tournees[i].nombre_de_fournisseurs; j++) {
+			monFlux << " " << tournees[i].fournisseurs;
+		}
+		monFlux << std::endl;
+	}
+
+	return 0;
+}
+
+
+
 
 int main()
 {
 	// PARSAGE
 
 	// TRAINTEMENT INITIAL
+	vector<tournee> tournees;
+	vector<tournee> groupe;
+	vector<int> fournisseurs_sous_traites;
 
 	// CALCUL 
-	cout << "Hello CMake." << endl;
 	return 0;
 }
--- RechOP.h
+++ RechOP.h
@@ -4,5 +4,20 @@
 #pragma once
 
 #include <iostream>
+#include "parseur/parseur.h"
+//#include "output.h"
 
 // TODO: Reference additional headers your program requires here.
+
+
+struct tournee {
+	int groupe;
+	int semaine;
+	int nombre_de_fournisseurs;
+	int fournisseurs[4];
+};
+
+struct groupe {
+	int nombre_de_fournisseurs;
+	int fournisseurs[4];
+};
